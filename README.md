010bd64fdae5727f9c594735f211e38d4929846f
mys1024 <mys.1024@outlook.com>
2022-08-19 16:03:25 +0800

    chore: git init

---
+++ README.md
@@ -0,0 +1,9 @@
+# base64-es
+
+A Base64 library in ES Module.
+
+## Reference
+
+- Base64: [RFC 4648: Base 64 Encoding](https://datatracker.ietf.org/doc/html/rfc4648#section-4)
+
+- Base64URL: [RFC 4648: Base 64 Encoding with URL and Filename Safe Alphabet](https://datatracker.ietf.org/doc/html/rfc4648#section-5)
---
+++ src/base64.ts
@@ -0,0 +1,49 @@
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+const indexes = new Map<string, number>()
+for (let i = 0; i <chars.length; i++) {
+  indexes.set(chars[i], i)
+}
+
+function encode(data: Uint8Array, padding = true): string {
+  let s = ''
+  let tmp = 0
+  for (let i = 0; i < data.length; i++) {
+    switch (i % 3) {
+      case 0:
+        s += chars[(data[i] & 0b11111100) >> 2]
+        tmp = (data[i] & 0b00000011) << 4
+        break
+      case 1:
+        s += chars[tmp | ((data[i] & 0b11110000) >> 4)]
+        tmp = (data[i] & 0b00001111) << 2
+        break
+      case 2:
+        s += chars[tmp | ((data[i] & 0b11000000) >> 6)]
+        s += chars[(data[i] & 0b00111111)]
+        tmp = 0
+    }
+  }
+  switch (data.length % 3) {
+    case 1:
+      s += chars[tmp]
+      if (padding)
+        s += '=='
+      break
+    case 2:
+      s += chars[tmp]
+      if (padding)
+        s += '='
+  }
+  return s
+}
+
+function decode(base64: string): Uint8Array {
+  const arr: number[] = []
+  // TODO
+  return new Uint8Array(arr)
+}
+
+export const Base64 = {
+  encode,
+  decode
+}
