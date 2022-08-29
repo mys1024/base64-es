49d6b7af881b48e61ec4cd8906a3e8e9fde81bac
mys1024 <mys.1024@outlook.com>
2022-08-30 01:47:12 +0800

    chore: test

---
+++ test/base64.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, it } from 'vitest'
+import { Base64 } from '../src/base64.js'
+
+describe('Base64', () => {
+  it('encode with padding', () => {
+    const textEncoder = new TextEncoder()
+    expect(
+      Base64.encode(new Uint8Array([77, 203])),
+    ).toMatchSnapshot()
+    expect(
+      Base64.encode(new Uint8Array([80, 171, 243, 128])),
+    ).toMatchSnapshot()
+    expect(
+      Base64.encode(textEncoder.encode('Hello, world!')),
+    ).toMatchSnapshot()
+    expect(
+      Base64.encode(textEncoder.encode('你好，🌏！这是一个处理 Base64 的库。')),
+    ).toMatchSnapshot()
+  })
+
+  it('encode without padding', () => {
+    const textEncoder = new TextEncoder()
+    expect(
+      Base64.encode(new Uint8Array([77, 203]), false),
+    ).toMatchSnapshot()
+    expect(
+      Base64.encode(new Uint8Array([80, 171, 243, 128]), false),
+    ).toMatchSnapshot()
+    expect(
+      Base64.encode(textEncoder.encode('Hello, world!'), false),
+    ).toMatchSnapshot()
+    expect(
+      Base64.encode(textEncoder.encode('你好，🌏！这是一个处理 Base64 的库。'), false),
+    ).toMatchSnapshot()
+  })
+
+  it('decode with padding', () => {
+    const textEncoder = new TextEncoder()
+    expect(
+      Base64.decode(Base64.encode(new Uint8Array([80, 171, 243, 128]))),
+    ).toMatchSnapshot()
+    expect(
+      Base64.decode(Base64.encode(textEncoder.encode('Hello, world!'))),
+    ).toMatchSnapshot()
+  })
+
+  it('decode without padding', () => {
+    const textEncoder = new TextEncoder()
+    expect(
+      Base64.decode(Base64.encode(new Uint8Array([80, 171, 243, 128]), false)),
+    ).toMatchSnapshot()
+    expect(
+      Base64.decode(Base64.encode(textEncoder.encode('Hello, world!'), false)),
+    ).toMatchSnapshot()
+  })
+})
